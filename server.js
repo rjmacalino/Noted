9e327a6d35807f36445dcef47d6c7777773e74aa
rjmacalino <rjhaymacalino@gmail.com>
2023-05-11 13:45:26 +1000

    initial commit for adding mongodb

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3500;
-const path = require('path');
-const { logger } = require('./middleware/logger');
-const errorHandler = require('./middleware/errorHandler');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+require ('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3500
+const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
+console.log(`${process.env.NODE_ENV} mode`)
 
-app.use(logger);
+app.use(logger)
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')))
 
-
-app.use('/', require('./routes/root'));
-
+app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
         return;
     }
     else if (req.accepts('json')) {
-        res.json({ message: '404 Not found' });
+        res.json({ message: '404 Not found' })
         return;
     }
     else {
-        res.type('txt').send('404 Not found');
+        res.type('txt').send('404 Not found')
     }
 });
 
-
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+    console.log(`Server is listening on port ${PORT}`)
+})
